240e060b62894214cf760e52da5c736f8b2b9f28
yunjae62 <wodbs7758@naver.com>
2024-10-03 15:34:25 +0900

    feat: Stock 엔티티 추가

---
+++ src/main/java/ex/stock/domain/Stock.java
@@ -0,0 +1,38 @@
+package ex.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "stocks")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고가 부족합니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+}
